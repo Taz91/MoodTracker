c8e5cdcb76c1aa434274f09e03596fcca074c991
Taz91 <sylvain.chazette@orange.fr>
2018-12-10 22:43:09 +0100

    pb gradle

--- app/src/main/java/com/agilya/syc/moodtracker/MainActivity.java
+++ app/src/main/java/com/agilya/syc/moodtracker/MainActivity.java
@@ -1,36 +1,49 @@
 package com.agilya.syc.moodtracker;
 
-import android.app.Activity;
-//----
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
+import android.view.Gravity;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-
 public class MainActivity extends AppCompatActivity {
+    //for swipe and background
+    @BindView(R.id.accueilid) RelativeLayout accueilid;
+    @BindView(R.id.btnComment) ImageButton btnComment;
+    @BindView(R.id.btnHistory) ImageButton btnHistory;
+    // imageview vairable to vary screen
+    @BindView(R.id.imageHumeur) ImageView imgImageHumeur;
 
-    @BindView(R.id.tvSwipDescription) TextView tvSwipDescription;
 
     private ImageView mImageView;
-    private int tableauChaine[] = {R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
-    private int index = 2 ;
+    // Smiley : use tbl to show picture in swipe context
+    private int tbliSmiley[] = {R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
+    private int index = 3 ;
 
-    //---
-    SharedPreferences sharedPreferences;
-    private static final String PREFS = "PREFS";
-    private static final String PREFS_AGE = "PREFS_AGE";
-    private static final String PREFS_NAME = "PREFS_NAME";
+    // Color : use color.xlm to define background screen
+    private int tbliColor[] = {R.color.faded_red,R.color.warm_grey,R.color.cornflower_blue_65,R.color.light_sage,R.color.banana_yellow };
 
+    // History mood use SharedPreferences
+    private SharedPreferences sharedPreferencesHistory;
+    //File
+    private static final String PREFS_HISTORY = "History";
+    //key
+    private static final String PREFS_DAY = "DayMood";
+    private static final String PREFS_COMMENT = "DayComment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,88 +51,150 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        //set resource for imageview by default
+        mImageView.setImageResource(tbliSmiley[index]);
 
-        //bind imageview with your xml's id
-        mImageView = (ImageView)findViewById(R.id.imageHumeur);
-        //set resource for imageview
-        mImageView.setImageResource(tableauChaine[index]);
+        //-------------- load sharedPreferences and use for history
+        sharedPreferencesHistory = getBaseContext().getSharedPreferences(PREFS_HISTORY, MODE_PRIVATE);
 
-
-
-        sharedPreferences = getBaseContext().getSharedPreferences(PREFS, MODE_PRIVATE);
-        //pour cela, on commence par regarder si on a déjà des éléments sauvegardés
-        if (sharedPreferences.contains(PREFS_AGE) && sharedPreferences.contains(PREFS_NAME)) {
-
-            int age = sharedPreferences.getInt(PREFS_AGE, 0);
-            String name = sharedPreferences.getString(PREFS_NAME, null);
-
-            Toast.makeText(this, "Age: " + age + " name: " + name, Toast.LENGTH_SHORT).show();
-
-        } else {
-            //si aucun utilisateur n'est sauvegardé, on ajouter [24,florent]
-            sharedPreferences
-                    .edit()
-                    .putInt(PREFS_AGE, 24)
-                    .putString(PREFS_NAME, "florent")
-                    .apply();
-
-            Toast.makeText(this, "Sauvegardé, relancez l'application pour voir le résultat", Toast.LENGTH_SHORT).show();
-        }
-
-
-
-
-
-
-
-        tvSwipDescription.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+        accueilid.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            // show next up smiley
             public void onSwipeTop() {
-                //Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastTop), Toast.LENGTH_SHORT).show();
-                if (index + 1 <= tableauChaine.length)
+                if (index + 1 <= tbliSmiley.length - 1 )
                 {
                     index++ ;
-                    mImageView.setImageResource(tableauChaine[index]);
+                    // apply mood selected
+                    f_applyMoodDay();
                 }
-
-
             }
-            public void onSwipeRight(MotionEvent e1) {
-                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastRight), Toast.LENGTH_SHORT).show();
-            }
-            public void onSwipeLeft() {
-                //Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastLeft), Toast.LENGTH_SHORT).show();
-                if (index + 1 <= tableauChaine.length)
+            // show next down smiley
+            public void onSwipeBottom() {
+                if ( index - 1 >= 0 )
                 {
                     index-- ;
-                    mImageView.setImageResource(tableauChaine[index]);
+                    // apply mood selected
+                    f_applyMoodDay();
                 }
-            }
-            public void onSwipeBottom() {
-                Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastBottom), Toast.LENGTH_SHORT).show();
-            }
-
-
-            public void onSwipe(String p_s_mvt_e1, String p_s_mvt_e2, float Xe1, float Ye1, float Xe2, float Ye2){
-                tvSwipDescription.setText(
-                        tvSwipDescription.getText() + "\n"
-                                + p_s_mvt_e1 + " - X = " + Xe1 + " - Y = " + Ye1  + "\n"
-                                + p_s_mvt_e2 + " - X = " + Xe2 + " - Y = " + Ye2  + "\n"
-                );
             }
         });
 
 }
 
-    /*
-    private void initializeView() {
-        tvSwipDescription=(TextView) findViewById(R.id.tvSwipDescription);
+    @OnClick(R.id.btnComment)
+    void f_history(){
+        // enregistrement du jour + etat et décaler les jours présents
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String keynowFormated = format.format(now);
+
+
+        // pourquoi ces 2 façon de faire ??
+
+        if (sharedPreferencesHistory.contains(keynowFormated)) {
+            //key exist, value must be modified
+            SharedPreferences.Editor editor = sharedPreferencesHistory.edit();
+            editor.putString(keynowFormated, index + "_" + "mon nouveau commentaire").commit();
+        }
+        else {
+            sharedPreferencesHistory.edit().putString(keynowFormated, index + "_" + "mon commentaire qui tue la mort " ).commit();
+        }
     }
-    */
-    @OnClick(R.id.button)
-    public void clickmoi (){
-        Toast.makeText(getApplicationContext(), getResources().getString(R.string.toastRight), Toast.LENGTH_SHORT).show();
+
+    @OnClick(R.id.btnHistory)
+    void f_showhistory(){
+        /*
+        les clés doivent être soit néttoyé soit filtrées sur la date.
+        vue à faire :
+        hauteurEcran = ( hauteurEcran - hauteurBtn )/7
+        */
+        // afficher si existe
+        Date now = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String keynowFormated = format.format(now);
+        if (sharedPreferencesHistory.contains(keynowFormated)){
+            Context context = getApplicationContext();
+            CharSequence text = sharedPreferencesHistory.getString(keynowFormated, "pas trouvé, mais c'est pas possible normalement !!!");
+            Toast.makeText(context, text, Toast.LENGTH_SHORT ).setGravity(Gravity.TOP | Gravity.LEFT,0 ,0);
+            Toast.makeText(context, text, Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    private void f_nextDay(){
+    // produire le décalage de jour, surement que jour 0 n'est pas utile, car on écrase jour 1, puis quand on décale jour 1 devient vide.
+
+        Map<String,?> keys = sharedPreferencesHistory.getAll();
+        for(Map.Entry<String,?> entry : keys.entrySet()){
+            //Log.d("map values",entry.getKey() + ": " + entry.getValue().toString());
+
+            if(true){
+                //sequenece de suppressoin:
+                SharedPreferences.Editor editor = sharedPreferencesHistory.edit();
+                editor.remove(entry.getKey()).commit();
+            }
+        }
+
+        /*
+
+        la key actu =>
+        J1 y a rien, je stocke avec la key
+        J1 y a qq chose, je vérifie si key existe, oui => modif si non => put
+
+        J1 à l'ouverture de l'appli
+            => faire un nettoyage des key périmées càd dire key > key +7 ,
+            => si key la plus petite < key actu => on déplace
+
+        */
     }
 
 
+    private void f_applyMoodDay(){
+        // Apply the mood's choice, image and background color
+        mImageView.setImageResource(tbliSmiley[index]);
+        accueilid.setBackgroundColor(getResources().getColor( tbliColor[index]));
 
+        //build the key
+        Date today = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String keynowFormated = format.format(today);
+
+        try {
+            Date maDate = format.parse("20181204");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        // pourquoi ces 2 façon de faire ??
+
+        //key exist, value must going by modified, else by only put.
+        if (sharedPreferencesHistory.contains(keynowFormated)) {
+            SharedPreferences.Editor editor = sharedPreferencesHistory.edit();
+            editor.putString(keynowFormated, index + "_" + "mon nouveau commentaire").commit();
+        }
+        else {
+            sharedPreferencesHistory.edit().putString(keynowFormated, index + "_" + "mon commentaire qui tue la mort " ).commit();
+        }
+    }
+    private void f_DoHistory(){
+        sharedPreferencesHistory
+                .edit()
+                .putString(PREFS_DAY, "20181207_4")
+                .putString(PREFS_COMMENT, "20181207_tjrs c super content que je suis")
+                .putString(PREFS_DAY, "20181206_4")
+                .putString(PREFS_COMMENT, "20181206_c super content que je suis")
+                .putString(PREFS_DAY, "20181205_2")
+                .putString(PREFS_COMMENT, "20181205_cv mieux")
+                .putString(PREFS_DAY, "20181204_1")
+                .putString(PREFS_COMMENT, "20181204_c bof")
+                .putString(PREFS_DAY, "20181203_0")
+                .putString(PREFS_COMMENT, "20181203_c a chié")
+                .putString(PREFS_DAY, "20181202_0")
+                .putString(PREFS_COMMENT, "20181202_c super à chier")
+                .putString(PREFS_DAY, "20181202_2")
+                .putString(PREFS_COMMENT, "20181202_cv")
+                .putString(PREFS_DAY, "20181201_2")
+                .putString(PREFS_COMMENT, "20181201_c pas mal")
+                .commit();
+    }
 }
+
